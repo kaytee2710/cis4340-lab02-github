d7701e0dcc1622abe0c65a5c9855d594cbe58909
kaytee2710 <138022214+kaytee2710@users.noreply.github.com>
2026-03-07 20:59:03 -0500

    Add R04_STR03_J.java

---
+++ R04_STR03_J.java
@@ -0,0 +1,44 @@
+/*
+Rule: STR03-J
+Do not encode noncharacter data as a string
+*/
+
+import java.math.BigInteger;
+
+public class R04_STR03_J {
+
+    public static void noncompliant() {
+
+        BigInteger x = new BigInteger("530500452766");
+
+        byte[] byteArray = x.toByteArray();
+
+        String s = new String(byteArray);
+
+        byteArray = s.getBytes();
+
+        x = new BigInteger(byteArray);
+
+        System.out.println(x);
+    }
+
+    public static void compliant() {
+
+        BigInteger x = new BigInteger("530500452766");
+
+        String s = x.toString();
+
+        byte[] byteArray = s.getBytes();
+
+        String ns = new String(byteArray);
+
+        x = new BigInteger(ns);
+
+        System.out.println(x);
+    }
+
+    public static void main(String[] args) {
+        noncompliant();
+        compliant();
+    }
+}
